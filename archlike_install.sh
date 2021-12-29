62980ebe5e43bc6c6650d55fcdd5e601adc0b5dc
Andy Smith <smith780@gmail.com>
2021-12-29 08:53:29 -0500

    Update location where script is saved

--- archlike_install.sh
+++ archlike_install.sh
@@ -8,5 +8,5 @@ echo "$name" > /tmp/user_name
 dialog --title "App and Dotfiles installation" --yesno \
     "Do you want to install all your apps and your dotfiles?" \
     10 60 \
-    && curl https://raw.githubusercontent.com/andyrsmith/linux_installer/main/install_arch_apps.sh > /tmp/install_apps.sh \
-    && sudo -u $name bash /tmp/install_apps.sh
+    && curl https://raw.githubusercontent.com/andyrsmith/linux_installer/main/install_arch_apps.sh > install_apps.sh \
+    && sudo -u $name bash install_apps.sh
